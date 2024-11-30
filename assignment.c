ce2e2df6815460c893793d142b5f26ae5f9f7265
Oipipa <sqpi86@gmail.com>
2024-11-30 13:32:59 +0100

    fixed according to workflow

--- assignment.c
+++ assignment.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[]) {
 
     // Check number of arguments
     if (argc - 1 != 2) {
-        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\n", argc - 1);
+        printf("Incorrect usage. You provided %d arguments. The correct number of arguments is 2\r", argc - 1);
         return 1;
     }
 
